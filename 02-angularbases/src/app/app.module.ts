f24faec951c1e6340b64eedaf066b29aa74224cb
roosevelt <ing.ospinagonzalez@gmail.com>
2019-03-05 18:06:24 -0500

    se acomoda el llamado a a las paginas en pages.module.ts

--- 02-angularbases/src/app/app.module.ts
+++ 02-angularbases/src/app/app.module.ts
@@ -2,23 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './pages/about/about.component';
+/* import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component'; */
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './component/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
+/*     AboutComponent,
     ContactComponent,
-    HomeComponent,
+    HomeComponent, */
     MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
